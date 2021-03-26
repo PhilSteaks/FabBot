77d9b6967912638642028612148939c5d40e44b8
PhilSteaks <github@chanp.org>
2021-03-25 23:58:20 -0700

    Read token from a file

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 # main.py
+import os
+import sys
 from fab_bot import FabBot
 
-k_bot_token = "<Insert Token>"
+
+def get_token():
+    """ Reads the token file and strips whitespace and newlines """
+    token = None
+    k_token_file_name = "token.txt"
+    file_path = os.path.join(os.path.dirname(__file__), k_token_file_name)
+
+    try:
+        with open(file_path) as token_file:
+            token = token_file.read().strip(" \t\n\r")
+    except FileNotFoundError:
+        print(
+            "{0} not found. Make sure it is in your current directory.".format(
+                k_token_file_name))
+    finally:
+        return token
+
 
 if __name__ == "__main__":
+    token = get_token()
+    if not token:
+        sys.exit()
+
     print("Starting Bot...")
     bot = FabBot()
     bot.register_cogs()
-    bot.run(k_bot_token)
+    bot.run(token)
     print("Stopped Bot")
