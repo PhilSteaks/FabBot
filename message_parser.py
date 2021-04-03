72f131b310d84bceafda2e471b3360e5be5b92f7
nli <nelsonli1015@gmail.com>
2021-04-03 03:56:01 -0700

    removed years ago

--- message_parser.py
+++ message_parser.py
@@ -9,13 +9,7 @@ class MessageParser():
     async def parse_command(self, message):
         if message.content == "test":
             await self.__parse_test(message)
-        if "years ago" in message.content.lower():
-            await self.__parse_years_ago(message)
         return
 
     async def __parse_test(self, message):
         await self.fab.send_message(message.channel, "Testing 1 2 3!")
-
-    async def __parse_years_ago(self, message):
-        await self.fab.send_message(
-            message.channel, "https://www.youtube.com/watch?v=r_WWkQgT3To")
